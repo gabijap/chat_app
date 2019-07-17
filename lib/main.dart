590e346ae3d3934c15724b72935fca704293db49
gabijap <g.petroskeviciute@gmail.com>
2019-07-17 11:18:17 +0300

    Implemented Google Sign In.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final GoogleSignIn googleSignIn = GoogleSignIn();
+
+/*  Use the google sign in data to authenticate a
+    FirebaseUser and then return that user.
+ */
+Future<String> signInWithGoogle() async {
+  final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+  final GoogleSignInAuthentication googleSignInAuthentication =
+      await googleSignInAccount.authentication;
+
+  final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleSignInAuthentication.accessToken,
+      idToken: googleSignInAuthentication.idToken,
+  );
+
+  final FirebaseUser user = await _auth.signInWithCredential(credential);
+
+  assert(!user.isAnonymous);
+  assert(await user.getIdToken() != null);
+
+  final FirebaseUser currentUser = await _auth.currentUser();
+  assert(user.uid == currentUser.uid);
+
+  return 'signInWithGoogle succeeded: $user';
+}
+
+/*  Sign out of the current Google account.*/
+void signOutGoogle() async{
+  await googleSignIn.signOut();
+  print("User Sign Out");
+}
 
 void main() => runApp(new ChatApp());
 
@@ -7,12 +43,55 @@ class ChatApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'ChatApp',
-      home: new ChatScreen(),
+      home: new LoginPage(),
     );
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: AppBar(
+          title: Text('Chat App'),
+          centerTitle: true,
+          backgroundColor: Colors.lime,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              GoogleSignInButton(
+                onPressed: () {
+                  signInWithGoogle().whenComplete(() {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                          builder: (context) {
+                            return ChatScreen();
+                          },
+                      ),
+                    );
+                  });
+                },
+              )
+            ],
+          ),
+        )
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  @override
+  State createState() => new ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
